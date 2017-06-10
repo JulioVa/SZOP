a5c0b80952287945b17cf2ab3a18af53be8059a9
Patryk Proniewicz <pproniew@gmail.com>
2017-06-10 16:23:26 +0200

    added post to communicate with webapp

--- raspberry/ds18b20.py
+++ raspberry/ds18b20.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import time
+import requests
 
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
@@ -9,11 +10,13 @@ base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
 
+
 def read_temp_raw():
     f = open(device_file, 'r')
     lines = f.readlines()
     f.close()
     return lines
+
 
 def read_temp():
     lines = read_temp_raw()
@@ -24,6 +27,9 @@ def read_temp():
     if equals_pos != -1:
         temp_string = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0
+        # url = 'will get url'
+        # query = {'field': temp_c}
+        # res = requests.post(url, data=query)
         return temp_c
 
 while True:
