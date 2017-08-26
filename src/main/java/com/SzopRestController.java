fed2fcabdc4f7f501b4386cc53358fad5b993bc3
KasiaGa <kasik2121@gmail.com>
2017-08-26 21:49:15 +0200

    Schema endpoints

--- src/main/java/com/SzopRestController.java
+++ src/main/java/com/SzopRestController.java
@@ -1,9 +1,13 @@
 package com;
 
+import com.database.dto.SchemaDto;
 import com.database.dto.SensorDto;
+import com.database.model.Schema;
 import com.database.model.Sensor;
 import com.database.model.Temperature;
+import com.database.service.SchemaService;
 import com.database.service.SensorService;
+import com.database.util.SchemaUtil;
 import com.database.util.SensorUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -49,13 +53,52 @@ public class SzopRestController {
     }
 
     @RequestMapping(value = "/system/{systemId}/sensors/{sensorId}", method = RequestMethod.PUT)
-    public ResponseEntity<SensorDto> getSensorsBySchemaId(@PathVariable int systemId, @PathVariable int sensorId, @RequestBody SensorDto sensorDto) {
+    public ResponseEntity<SensorDto> updateSensor(@PathVariable int systemId, @PathVariable int sensorId, @RequestBody SensorDto sensorDto) {
         Sensor sensor = SensorService.findBySensorIdAndSystemId(sensorId, systemId);
         if (sensor == null) {
             return ResponseEntity.notFound().build();
         }
         sensor = SensorUtil.updateSensor(sensor, sensorDto);
         SensorService.update(sensor);
+        return ResponseEntity.ok().build();
+    }
+
+    // schema
+    @RequestMapping(value = "/schema", method = RequestMethod.POST)
+    public ResponseEntity<SchemaDto> createSchema(@RequestBody SchemaDto schemaDto) {
+        Schema schema = SchemaUtil.addSchema(schemaDto);
+        SchemaService.save(schema);
+        return ResponseEntity.ok().body(schemaDto);
+    }
+
+    @RequestMapping(value = "/schema/{schemaId}", method = RequestMethod.GET)
+    public ResponseEntity<Schema> getSchemaById(@PathVariable int schemaId) {
+        Schema schema = SchemaService.findSchemaById(schemaId);
+        if (schema == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(schema);
+    }
+
+    @RequestMapping(value = "/schema/{schemaId}", method = RequestMethod.PUT)
+    public ResponseEntity<SensorDto> updateSchema(@PathVariable int schemaId, @RequestBody SchemaDto schemaDto) {
+        Schema schema = SchemaService.findSchemaById(schemaId);
+        if (schema == null) {
+            return ResponseEntity.notFound().build();
+        }
+        schema = SchemaUtil.updateSchema(schema, schemaDto);
+        SchemaService.update(schema);
+        return ResponseEntity.ok().build();
+    }
+
+
+    @RequestMapping(value = "/schema/{schemaId}", method = RequestMethod.DELETE)
+    public ResponseEntity<SensorDto> deleteSchema(@PathVariable int schemaId) {
+        Schema schema = SchemaService.findSchemaById(schemaId);
+        if (schema == null) {
+            return ResponseEntity.notFound().build();
+        }
+        SchemaService.delete(schema);
         return ResponseEntity.ok().build();
     }
 
---
+++ src/main/java/com/database/util/SchemaUtil.java
@@ -0,0 +1,20 @@
+package com.database.util;
+
+import com.database.dto.SchemaDto;
+import com.database.model.Schema;
+
+public class SchemaUtil {
+
+    public static Schema addSchema(SchemaDto schemaDto) {
+        Schema schema = new Schema();
+        schema.setName(schemaDto.getName());
+        schema.setImg(schemaDto.getImg());
+        return schema;
+    }
+
+    public static Schema updateSchema(Schema schema, SchemaDto schemaUpdate) {
+        schema.setName(NotNullUtil.setNotNull(schema.getName(), schemaUpdate.getName()));
+        schema.setImg(NotNullUtil.setNotNull(schema.getImg(), schemaUpdate.getImg()));
+        return schema;
+    }
+}
