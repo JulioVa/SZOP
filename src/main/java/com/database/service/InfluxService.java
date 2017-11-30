440cb51ed6dcee7535cb5890fb1ae34ec80e254f
KasiaGa <kasik2121@gmail.com>
2017-11-30 23:14:15 +0100

    User id fix

--- src/main/java/com/database/service/InfluxService.java
+++ src/main/java/com/database/service/InfluxService.java
@@ -41,7 +41,7 @@ public class InfluxService {
 
         for (TemperatureData temp : temps) {
 
-            Point point = Point.measurement("user_" + userId)
+            Point point = Point.measurement("user_" + user.getId())
                     .tag("type", temp.getType())
                     .tag("sensor", temp.getSensorId())
                     .tag("system", Integer.valueOf(sys.getId()).toString())
